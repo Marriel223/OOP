d125e02013b6aaa5fbd907cf0bc55241dbbfba09
Marriel223 <23-17177@g.batstate-u.edu.ph>
2024-09-20 16:17:33 +0800

    Create Activity.java

---
+++ Activity.java
@@ -0,0 +1,25 @@
+class Activiy
+ public static void main(String [] args) {
+
+    Dog puppy = new Dog();
+
+    puppy.setName(thisName:"browny");
+
+    System.out.println(puppy.showname(();
+    System.out.println();
+
+    puppy.isMale = true;
+
+
+    System.out.println(puppy.showname() + " is a Male: " + puppy.isMale);
+
+    System.out.println();
+    
+    puppy.bark();
+    System.out.println();
+
+    puppy.setList0fPuppies(new String[] {"Eliza","Uno","Dos","Tres"});
+
+    puppy.setNumOfPuppies(numOfPuppy:4);
+
+    
